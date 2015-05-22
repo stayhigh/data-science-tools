05835aa002830c65350a737a7c77230bc6931f24
stayhigh <stayhigh@github>
2015-05-23 00:45:42 +0800

    data science tools commandlines

---
+++ datatools_installer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#installer for data scientist
+brew install mawk
+sudo pip install awscli
+sudo pip install bigmler
+sudo pip install skll
+
+# csvkit is for csvcut, csvgrep, csvjoin, csvlook, csvsort, csvsql, csvstack, csvstat, in2csv, sql2csv,  
+
+sudo pip install csvkit
+
+brew install imagemagick
+brew install drake
+brew install jq
+brew install parallel
+brew install pbc
+brew install unrar
+go get github.com/jehiah/json2csv
+npm install xml2json-command
+git clone https://github.com/jeroenjanssens/data-science-at-the-command-line.git
+
+# coreutils is for shuf, sort, split, type, uniq, tee, tail, seq
+brew install coreutils 
+
